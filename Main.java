d5c8e24603da1e5b0015d652178c2949bfed77a8
eluli345 <ulisesbazzaro18@gmail.com>
2024-05-22 23:25:54 -0300

    yei

---
+++ Main.java
@@ -0,0 +1,14 @@
+package creditcard;
+
+public class Main {
+
+	public static void main(String[] args) {
+		transform t = new transform();
+		String txt = t.card("1040215321521");
+		
+		System.out.println(txt);
+		
+
+	}
+
+}
---
+++ transform.java
@@ -0,0 +1,25 @@
+package creditcard;
+
+public class transform {
+	
+	
+	public String card(String a) {
+        String numbercard = a;
+        int nc = numbercard.length();
+        int ncFinal = nc - 4;
+        StringBuilder r = new StringBuilder();
+        
+        for (int i = 0; i < nc; i++) {
+            char c = numbercard.charAt(i);
+                        
+            if (i < ncFinal) {
+                r.append('#');
+            } else {
+                r.append(c);
+            }
+        }
+        
+        return r.toString();
+    }
+}	
+	
